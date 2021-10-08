0db4a282433e0f1a580252f60f8f92238a27893e
Nehal Ahmad <52347191+Nehal98@users.noreply.github.com>
2021-10-08 21:11:28 +0530

    fetching random room id

--- src/components/CreateRoomForm/CreateRoomForm.js
+++ src/components/CreateRoomForm/CreateRoomForm.js
@@ -3,57 +3,73 @@ import Textarea from '../../components/TextArea/TextArea';
 import { Container, Row, Col, Button } from "react-bootstrap";
 import './CreateRoomForm.css'
 import {MdContentCopy} from 'react-icons/md'
-function CreateRoomForm(){
-    return(
+import React from 'react';
+
+export default class CreateRoomForm extends React.Component {
+
+  state = {
+    loading: true,
+    roomid: null
+  }
+
+  async componentDidMount(){
+    const url = "http://localhost:8080";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({roomid: data.value, loading: false });
+  }
+
+render() {
+return( 
         <div className='create-room-div'>
-        <Container fluid>
-          <Row sm={2} xs={1}>
-            <Col>
-              <h1>Create Room</h1>
-            </Col>
-            <Col className='d-flex justify-content-end'>
-              <Button size='lg' className='create-room-button'>
-                ROOM ID: THXQL <MdContentCopy/>
-              </Button>
-            </Col>
-          </Row>
-        </Container>
-        <Container fluid>
-          <Row xs={1} md={2}>
-            <Col>
-              <div className='input-div'>
-                <h3>Room Name:</h3>
-                <div className='text-div'>
-                  <input type='text' />
-                  <Button>SAVE</Button>
-                </div>
-              </div>
-
-              <div className='input-div'>
-                <h3> Passcode: </h3>
-                <div className='text-div'>
-                  <input type='password' />
-                  <Button><MdContentCopy/></Button>
-                </div>
-              </div>
-
-              <div className='input-div'>
-                <Button>GENERATE PASSCODE</Button>
-              </div>
-            </Col>
-            <Col>
-              <Participants title='Number of Participants' />
-              <Textarea title='Game Rules' />
-            </Col>
-          </Row>
-        </Container>
-        <div className='create-room-button-div'>
-          <Button className='create-room-button' size='lg' variant='primary'>
-            CREATE ROOM
+    <Container fluid>
+      <Row sm={2} xs={1}>
+        <Col>
+          <h1>Create Room</h1>
+        </Col>
+        <Col className='d-flex justify-content-end'>
+          <Button size='lg' className='create-room-button'>
+            ROOM ID: {this.state.roomid} <MdContentCopy />
           </Button>
-        </div>
-      </div>
-    )
+        </Col>
+      </Row>
+    </Container>
+    <Container fluid>
+      <Row xs={1} md={2}>
+        <Col>
+          <div className='input-div'>
+            <h3>Room Name:</h3>
+            <div className='text-div'>
+              <input type='text' />
+              <Button>SAVE</Button>
+            </div>
+          </div>
+
+          <div className='input-div'>
+            <h3> Passcode: </h3>
+            <div className='text-div'>
+              <input type='password' />
+              <Button><MdContentCopy /></Button>
+            </div>
+          </div>
+
+          <div className='input-div'>
+            <Button>GENERATE PASSCODE</Button>
+          </div>
+        </Col>
+        <Col>
+          <Participants title='Number of Participants' />
+          <Textarea title='Game Rules' />
+        </Col>
+      </Row>
+    </Container>
+    <div className='create-room-button-div'>
+      <Button className='create-room-button' size='lg' variant='primary'>
+        CREATE ROOM
+      </Button>
+    </div>
+  </div>
+    );
 }
 
-export default CreateRoomForm
+}
